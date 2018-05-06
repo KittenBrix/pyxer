09b462d8de81dfc565079b063baa8833a354bd66
KittenBrix <lpquotec@gmail.com>
2018-05-05 23:52:41 -0700

    beginning definitions for WaveFormFunctions and Samples, which use WaveFormFunctions

--- Instrument/Sample.py
+++ Instrument/Sample.py
@@ -20,16 +20,102 @@ import numpy, sys, pyaudio, wav
 defines how a "Sample" should be used within an instrument.
 '''
 class Sample:
-    #set these per song
+    #set these per song per note
     beatsPerMinute = 120    #common bpm
-    sampleRate = 44100      #most common sample rate
-    noteDuration = 1/4      #quarter note duration.
+    sampleRate = 44100      #most common sample rate. Sample never changes it.
+    noteDuration = 1        #note duration. defaults to quarter notes. 1 = quarter note.
     A4 = 440                #base frequency for establishing pitch.
-    # [function definition (int frame, int )]
+    frequency = A4          #note frequency
+
+    #beatLengthSeconds is the literal number of seconds the sample lasts for.
+    #it's based on the note type (quarter/half/eight/etc) and the bpm.
+    def beatLengthSeconds(self):
+        # (S/M) / (B/M) * Beats = S/M*M/B*B = Seconds
+        return float(60.0/float(self.beatsPerMinute) * float(self.noteDuration))
+
+    #beatRateLength is the number of samples we write to. eg. if we have 60 bpm,
+    # it means a quarter note takes 1 second worth of time, which is multiplied
+    # by the sample rate to determine how many samples need to be written.
+    def beatRateLength(self):
+        #samplerate = frames per second
+        #beatlengthseconds = Seconds
+        # F/S * S = Frames
+        return int(this.sampleRate * this.beatLengthSeconds())
+
+    # [function definition (self, int frame, long currentframetotal)]
     waveFormsFuncs = []
     # literal array holding sample's play data
-    WaveForm = []
+    waveForm = []
 
-    def __init__(self, frequency = '440', songParams = [120,44100] ):
+    def __init__(self, frequency = '440', duration  = 1.0, songParams = [60,44100] ):
+        self.beatsPerMinute = songParams[0]
+        self.sampleRate=songParams[1]
+        self.frequency = frequency
+        self.noteDuration = duration
 
-    def
+
+    def writeToWaveForm(self, previousFrames = 0):
+        '''
+        for each frame, execute waveformfuncs and add them to the current frame, reducing volume based on number of funcs.
+        pass in previous frames so that the func can use them if need be for matching previous note end.
+        '''
+        vol = 1.0/len(self.waveFormsFuncs)
+        for i in range(self.beatRateLength()):
+            for func in self.waveFormsFuncs:
+                self.waveForm[i] += vol * func(self, i, previousFrames + i)
+
+
+
+
+
+
+    '''
+        frame = current frame on the note.
+        currentframe = within the context of the song, this is the total number of frames so far.
+    '''
+
+
+
+class WaveFormFunction:
+    volFunction = getConstantVol(1)
+    playFunction = SimpleSine
+    
+    frequency = 440
+
+
+
+    def __init__(self):
+
+
+
+
+
+
+
+
+    def getConstantVol(volume):
+        def getConstantVolume():
+            return volume
+        return getConstantVolume
+
+
+
+
+
+#simple sine. produces a sin wave frame value based on frame number, frequency, and sample rate
+def SimpleSine(frequency,frame, rate):
+    return math.sin(frequency * frame * (2* math.pi / rate))
+
+#simple square wave. output ranges between 0 and 1
+def SimpleSquare(frequency, frame, rate):
+    return 1.0 if (math.sin(frequency * frame * 2 * math.pi / rate) > 0) else 0
+
+
+def SimpleRectangle(frequency, frame, rate, cutoff = math.sin(math.pi / 4)):
+    return 1.0 if (math.sin(frequency * frame * 2 * math.pi / rate) > cutoff) else 0
+
+
+
+
+
+#
